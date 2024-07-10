1b1624d00452d799ace2802dd4daebee4f521956
hendisantika <hendisantika@yahoo.co.id>
2024-07-11 06:21:53 +0700

    Add saveData in LoadDataIntoDB🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/loaddata/service/LoadDataIntoDB.java
+++ src/main/java/id/my/hendisantika/loaddata/service/LoadDataIntoDB.java
@@ -22,4 +22,12 @@ public class LoadDataIntoDB {
 
     private final int batch = 1000;
     private final int start = 0;
+
+    public void saveData() {
+        while(start < 100000) {
+            List<Student> student = getNextStudentBatch(start);
+            start+=batch;
+            dbRepo.saveAll(student);
+        }
+    }
 }
