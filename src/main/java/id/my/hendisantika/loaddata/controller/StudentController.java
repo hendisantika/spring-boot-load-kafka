e83cd555457cf64b1da28391be3c6e110ae61aef
hendisantika <hendisantika@yahoo.co.id>
2024-07-11 06:26:52 +0700

    Add save in StudentController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/loaddata/controller/StudentController.java
+++ src/main/java/id/my/hendisantika/loaddata/controller/StudentController.java
@@ -3,9 +3,13 @@ package id.my.hendisantika.loaddata.controller;
 import id.my.hendisantika.loaddata.entity.Student;
 import id.my.hendisantika.loaddata.service.LoadDataIntoDB;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
 
 /**
  * Created by IntelliJ IDEA.
@@ -17,11 +21,19 @@ import org.springframework.web.bind.annotation.RestController;
  * Time: 06:25
  * To change this template use File | Settings | File Templates.
  */
-
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class StudentController {
     private final LoadDataIntoDB ldb;
     private final KafkaTemplate<String, Student> template;
     private final NewTopic topic;
+
+    @GetMapping("/save")
+    public void save() {
+        log.info("---> Starting synchronous data load at {}", Instant.now());
+        ldb.saveData();
+        log.info("---> Synchronous data load completed at {}", Instant.now());
+    }
+
 }
