6b8f61b18d6b7fd6121a6b7572bd407ebbd494b5
hendisantika <hendisantika@yahoo.co.id>
2024-07-11 06:37:32 +0700

    Add Spring Doc API annotation🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/loaddata/controller/StudentController.java
+++ src/main/java/id/my/hendisantika/loaddata/controller/StudentController.java
@@ -2,6 +2,11 @@ package id.my.hendisantika.loaddata.controller;
 
 import id.my.hendisantika.loaddata.entity.Student;
 import id.my.hendisantika.loaddata.service.LoadDataIntoDB;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.NewTopic;
@@ -25,12 +30,30 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Student", description = "Endpoint for managing Student")
 public class StudentController {
     private final LoadDataIntoDB ldb;
     private final KafkaTemplate<String, Student> template;
     private final NewTopic topic;
 
     @GetMapping("/save")
+    @Operation(
+            summary = "Init Student Data",
+            description = "Init Student Data.",
+            tags = {"Student"})
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    description = "Success",
+                    responseCode = "200",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation =
+                            Student.class))
+            ),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Not found", responseCode = "404",
+                    content = @Content),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Internal error", responseCode = "500"
+                    , content = @Content)
+    }
+    )
     public void save() {
         log.info("---> Starting synchronous data load at {}", Instant.now());
         ldb.saveData();
@@ -38,6 +61,23 @@ public class StudentController {
     }
 
     @GetMapping("/send-to-kafka-broker")
+    @Operation(
+            summary = "Send Student Data to KAFKA",
+            description = "Send Student Data to KAFKA.",
+            tags = {"Tutorial"})
+    @ApiResponses(value = {
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    description = "Success",
+                    responseCode = "200",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation =
+                            Student.class))
+            ),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Not found", responseCode = "404",
+                    content = @Content),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(description = "Internal error", responseCode = "500"
+                    , content = @Content)
+    }
+    )
     public void sendToKafka() {
         List<Student> list = ldb.fetchAll();
         list.stream().forEach(x -> template.send(topic.name(),x));
