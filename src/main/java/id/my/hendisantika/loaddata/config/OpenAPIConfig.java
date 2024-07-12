a15dcef44bbd629795f7cb07decf49c7fb2418f5
hendisantika <hendisantika@yahoo.co.id>
2024-07-12 16:13:37 +0700

    Bug Fix issue 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/loaddata/config/OpenAPIConfig.java
+++ src/main/java/id/my/hendisantika/loaddata/config/OpenAPIConfig.java
@@ -1,6 +1,7 @@
 package id.my.hendisantika.loaddata.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
